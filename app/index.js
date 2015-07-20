38f70c6df4651a914a568eca1f010bf9daff253e
Ivan Bessarabov <ivan@bessarabov.ru>
2015-07-20 22:53:43 +0300

    Added /list.json

--- app/index.js
+++ app/index.js
@@ -1,11 +1,32 @@
 var express = require('express');
+var request = require('request');
+
+var app = express();
 
 var PORT = 80;
 
-var app = express();
 app.get('/', function (req, res) {
     res.send('Hello world\n');
 });
 
+app.get('/list.json', function (req, res) {
+    var answer;
+    var options = {
+        url: process.env.SERVER + "/api/1/get_all",
+        headers: {
+            'Authorization' : 'TOKEN key="' + process.env.TOKEN + '"',
+            'X-Requested-With' : 'XMLHttpRequest',
+        }
+    };
+
+    request(options, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            answer = JSON.parse(body);
+        }
+
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify(answer.result.objects));
+    })
+});
+
 app.listen(PORT);
-console.log('Running on http://localhost:' + PORT);
